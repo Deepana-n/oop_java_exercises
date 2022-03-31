3fa362e1bde6967ecafe1d3dcbaab17f982858a7
Deepana Naidu <deepana.naidu@gmail.com>
2022-03-31 23:23:56 +0100

    feedTheLion() test method passed

--- src/test/java/com/techreturners/cats/CatTest.java
+++ src/test/java/com/techreturners/cats/CatTest.java
@@ -45,13 +45,13 @@ public class CatTest {
         assertEquals(1100, lionCat.getAverageHeight());
     }
 
-//
-//    @Test
-//    public void feedTheLion() {
-//        Cat lionCat = new LionCat();
-//        assertEquals("Roar!!!!", lionCat.eat());
-//    }
-//
+
+    @Test
+    public void feedTheLion() {
+        Cat lionCat = new LionCat();
+        assertEquals("Roar!!!!", lionCat.eat());
+    }
+
 //    @Test
 //    public void feedTheCheetah() {
 //        Cat cheetahCat = new CheetahCat();
